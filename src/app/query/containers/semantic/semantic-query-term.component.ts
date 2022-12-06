04dd3637c063f00e78fb864bc48c98b63568805e
Ashmita Raju <ashmita.raju@gmail.com>
2022-12-06 12:02:02 -0500

    side bar changes

--- src/app/query/containers/semantic/semantic-query-term.component.ts
+++ src/app/query/containers/semantic/semantic-query-term.component.ts
@@ -6,7 +6,14 @@ import {first} from 'rxjs/operators';
 import {SemanticSketchDialogComponent} from './semantic-sketch-dialog.component';
 import {SemanticQueryTerm} from '../../../shared/model/queries/semantic/semantic-query-term.model';
 import {SemanticMap} from '../../../shared/model/queries/semantic/semantic-map.model';
+import {BehaviorSubject, combineLatest, Observable} from 'rxjs';
+import { SemanticCategory } from 'app/shared/model/queries/semantic/semantic-category.model';
+import {FormControl} from '@angular/forms';
 
+// import {AfterViewInit, Component, Inject, OnInit, Optional, ViewChild} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {SketchCanvasComponent} from '../../../shared/components/sketch/sketch-canvas.component';
+import {map, startWith} from 'rxjs/operators';
 @Component({
   selector: 'app-qt-semantic',
   templateUrl: 'semantic-query-term.component.html',
@@ -18,11 +25,54 @@ export class SemanticQueryTermComponent implements OnInit {
   @ViewChild('previewimg', {static: true})
   private previewimg: any;
 
+  private _categories: SemanticCategory[] = SemanticCategory.LIST;
+
+  public readonly formCtrl = new FormControl();
+
   /** The SemanticQueryTerm object associated with this SemanticQueryTermComponent. That object holds all the query-settings. */
   @Input()
   private semanticTerm: SemanticQueryTerm;
 
+  /** Currently selected SemanticCategory (pencil) */
+  private _selected: SemanticCategory = this._categories[0];
+  _usedSet: Set<SemanticCategory> = new Set();
+
+  private _used: BehaviorSubject<SemanticCategory[]> = new BehaviorSubject([]);
+  
+
+  readonly _filteredCategories: Observable<SemanticCategory[]>;
+
   constructor(private _dialog: MatDialog, private _resolver: ResolverService, private _http: HttpClient) {
+    const filtered = this.formCtrl.valueChanges.pipe(
+      startWith(''),
+      map(filter => filter.toLowerCase()),
+      map(filter => {
+        if (filter) {
+          return this._categories.filter(v => v.name.toLowerCase().indexOf(filter) === 0);
+        } else {
+          return this._categories;
+        }
+      })
+    );
+    this._filteredCategories = combineLatest([filtered, this._used]).pipe(
+        map((array: [SemanticCategory[], SemanticCategory[]]) => {
+        return array[0].sort((a, b) => {
+          const ia = array[1].indexOf(a);
+          const ib = array[1].indexOf(b);
+          if (ia === -1 && ib !== -1) {
+            return 1;
+          } else if (ia !== -1 && ib === -1) {
+            return -1;
+          } else if (a.name < b.name) {
+            return -1
+          } else if (a.name > b.name) {
+            return 1;
+          } else {
+            return 0;
+          }
+        })
+      })
+    );
   }
 
 
@@ -41,6 +91,25 @@ export class SemanticQueryTermComponent implements OnInit {
       this.openSketchDialog(new SemanticMap(this.semanticTerm.image, this.semanticTerm.map));
     } else {
       this.openSketchDialog();
+    }
+  }
+
+  public onItemSelected(selection: SemanticCategory) {
+    this._selected = selection;
+   //this._sketchpad.setActiveColor(selection.color);
+    const arr = this._used.getValue().concat().slice();
+    if (arr.indexOf(selection) === -1) {
+      arr.push(selection);
+      this._used.next(arr);
+    }
+  }
+
+  public onOptionSelected(selected: string) {
+    for (const category of this._categories) {
+      if (category.name === selected) {
+        this.onItemSelected(category);
+        break;
+      }
     }
   }
 
